f42c3f4509abe5626da14a631ff83b86bb3af125
dypool01 <dypool01@wsc.edu>
2022-11-03 14:44:56 -0500

    Contact Functions

--- Hogwarts.UI.MVC/Controllers/HomeController.cs
+++ Hogwarts.UI.MVC/Controllers/HomeController.cs
@@ -23,10 +23,7 @@ namespace Hogwarts.UI.MVC.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            return View();
-        }
+     
 
 
 
